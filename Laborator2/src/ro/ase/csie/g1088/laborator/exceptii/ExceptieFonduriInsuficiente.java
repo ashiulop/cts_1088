8ea5e0c69b06652f4411b44caf56478db43f4a79
ashiulop <catalin.vornicu707@gmail.com>
2021-03-04 12:23:40 +0200

    lab 2

---
+++ Laborator2/src/ro/ase/csie/g1088/laborator/exceptii/ExceptieFonduriInsuficiente.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.g1088.laborator.exceptii;
+
+public class ExceptieFonduriInsuficiente extends Exception{
+
+    public ExceptieFonduriInsuficiente(String message) {
+        super(message);
+    }
+
+    public ExceptieFonduriInsuficiente() {
+
+    }
+}
---
+++ Laborator2/src/ro/ase/csie/g1088/laborator/exceptii/ExceptieTransferIlegal.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.g1088.laborator.exceptii;
+
+public class ExceptieTransferIlegal extends Exception{
+    public ExceptieTransferIlegal(String message) {
+        super(message);
+    }
+
+    public ExceptieTransferIlegal() {
+    }
+}
