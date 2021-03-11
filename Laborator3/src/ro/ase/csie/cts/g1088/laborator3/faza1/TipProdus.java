68e2c178d4a0c66aaf99ceadf0996ce0101f2abe
ashiulop <catalin.vornicu707@gmail.com>
2021-03-11 12:35:26 +0200

    Update inainte de pauza

---
+++ Laborator3/src/ro/ase/csie/cts/g1088/laborator3/faza1/TipProdus.java
@@ -0,0 +1,18 @@
+package ro.ase.csie.cts.g1088.laborator3.faza1;
+
+public enum TipProdus {
+    NOU(0),
+    DISCOUNT(0.1f),
+    STOC_LIMITAT(0.25f),
+    VECHI(0.35f);
+
+    private final float discount;
+
+    TipProdus(float discount) {
+        this.discount = discount;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+}
