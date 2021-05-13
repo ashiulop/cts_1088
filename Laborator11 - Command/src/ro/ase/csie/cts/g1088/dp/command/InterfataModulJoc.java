be49c88c217a441cd7479cba7eb1d1a7fd919afe
ashiulop <catalin.vornicu707@gmail.com>
2021-05-13 13:25:07 +0300

    Commit final

---
+++ Laborator11 - Command/src/ro/ase/csie/cts/g1088/dp/command/InterfataModulJoc.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1088.dp.command;
+
+public interface InterfataModulJoc {
+    public void executaTask(String detalii);
+}
---
+++ Laborator11 - Command/src/ro/ase/csie/cts/g1088/dp/command/InterfataTaskAsincron.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1088.dp.command;
+
+public interface InterfataTaskAsincron {
+    public void startTaskAsincron();
+}
